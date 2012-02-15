2b264e0aa47873209ddd44b21297f1371e6763ed
WiziaWeb <wiziaweb@gmail.com>
2012-02-15 17:58:30 +0100

    Init

--- app/classes/controller/editor.php
+++ app/classes/controller/editor.php
@@ -20,7 +20,7 @@ class Controller_Editor extends Controller
 	 */
 	public function action_index()
 	{
-	    return View::forge('app/index');
+    return View::forge('app/index');
                 //return Response::forge(View::forge('welcome/index'));
 	}
 
